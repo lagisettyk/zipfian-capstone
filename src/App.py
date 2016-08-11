9c3a0e0110de59c34961cf5e08a078e0340459e2
Kiran Lagisetty <lagisettyk@Kirans-MacBook-Pro.local>
2016-08-11 11:49:47 -0700

    Working version 11-AUG-11:49

--- src/App.py
+++ src/App.py
@@ -37,13 +37,66 @@ def getLocationMap():
     print request
     return jsonify([locationMap])
 
-# @app.route('/new', methods=['POST'] )
-# def testform():
-#     # get data from request form, the key is the name you set in your form
-#     data = request.form['user_input']
-#     print data
-#     return redirect(url_for('mapview', messages={"Latitude": 1234, "Longitude": 5876}))
-
+def buildMap(suggestions):
+    if len(suggestions) >= 5:
+        print suggestions['Venue_name'].values[0], suggestions['Venue_name'].values[1]
+        sndmap = Map(
+            identifier="sndmap",
+            lat= suggestions['latitude'].values[0],
+            lng= suggestions['longitude'].values[0],
+            style="height:500px;width:750px;margin:0;",
+            collapsible= True,
+            markers=[
+              {
+                 'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+                 'lat': suggestions['latitude'].values[0],
+                 'lng': suggestions['longitude'].values[0],
+                 'infobox': "<b>"+suggestions['Venue_name'].values[0]+"</b>"
+              },
+              {
+                 'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                 'lat': suggestions['latitude'].values[1],
+                 'lng': suggestions['longitude'].values[1],
+                 'infobox': "<b>"+suggestions['Venue_name'].values[1]+"</b>"
+              },
+             {
+               'icon': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+               'lat': suggestions['latitude'].values[2],
+               'lng': suggestions['longitude'].values[2],
+               'infobox': "<b>"+suggestions['Venue_name'].values[2]+"</b>"
+             },
+              {
+                'icon': 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
+                'lat': suggestions['latitude'].values[3],
+                'lng': suggestions['longitude'].values[3],
+                'infobox': "<b>"+suggestions['Venue_name'].values[3]+"</b>"
+              },
+               {
+                 'icon': 'http://maps.google.com/mapfiles/ms/icons/orange-dot.png',
+                 'lat':  suggestions['latitude'].values[4],
+                 'lng': suggestions['longitude'].values[4],
+                 'infobox': "<b>"+suggestions['Venue_name'].values[4]+"</b>"
+               }
+            ]
+        )
+    else:
+        print "NO LOCATION HISTORY for this user....: ", len(suggestions)
+        sndmap = Map(
+            identifier="sndmap",
+            lat= 33.842623,
+            lng= -118.288384079933,
+            style="height:500px;width:750px;margin:0;",
+            collapsible= True,
+            markers=[
+              {
+                 'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+                 'lat': 33.842623,
+                 'lng': -118.288384079933,
+                 'infobox': "<b>"+"No location history for this user...."+"</b>"
+              }
+            ]
+        )
+    return sndmap
 
 @app.route("/analysis", methods=['POST','GET'])
 def mapview():
@@ -55,64 +108,15 @@ def mapview():
         userid = int(request.form["userid"])
         print latitude, longitude, userid
         suggestions = service.run(latitude, longitude, userid)
-        if len(suggestions) >= 5:
-            print suggestions['Venue_name'].values[0], suggestions['Venue_name'].values[1]
-            sndmap = Map(
-                identifier="sndmap",
-                lat= suggestions['latitude'].values[0],
-                lng= suggestions['longitude'].values[0],
-                style="height:500px;width:750px;margin:0;",
-                collapsible= True,
-                markers=[
-                  {
-                     'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-                     'lat': suggestions['latitude'].values[0],
-                     'lng': suggestions['longitude'].values[0],
-                     'infobox': "<b>"+suggestions['Venue_name'].values[0]+"</b>"
-                  },
-                  {
-                     'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-                     'lat': suggestions['latitude'].values[1],
-                     'lng': suggestions['longitude'].values[1],
-                     'infobox': "<b>"+suggestions['Venue_name'].values[1]+"</b>"
-                  },
-                 {
-                   'icon': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
-                   'lat': suggestions['latitude'].values[2],
-                   'lng': suggestions['longitude'].values[2],
-                   'infobox': "<b>"+suggestions['Venue_name'].values[2]+"</b>"
-                 },
-                  {
-                    'icon': 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
-                    'lat': suggestions['latitude'].values[3],
-                    'lng': suggestions['longitude'].values[3],
-                    'infobox': "<b>"+suggestions['Venue_name'].values[3]+"</b>"
-                  },
-                   {
-                     'icon': 'http://maps.google.com/mapfiles/ms/icons/orange-dot.png',
-                     'lat':  suggestions['latitude'].values[4],
-                     'lng': suggestions['longitude'].values[4],
-                     'infobox': "<b>"+suggestions['Venue_name'].values[4]+"</b>"
-                   }
-                ]
-            )
-        else:
-            print "NO LOCATION HISTORY for this user....: ", len(suggestions)
-            sndmap = Map(
-                identifier="sndmap",
-                lat= 33.842623,
-                lng= -118.288384079933,
-                style="height:500px;width:750px;margin:0;",
-                collapsible= True,
-                markers=[
-                  {
-                     'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-                     'lat': 33.842623,
-                     'lng': -118.288384079933,
-                     'infobox': "<b>"+"No location history for this user...."+"</b>"
-                  }
-                ]
-            )
+        sndmap = buildMap(suggestions)
+        return render_template('recommend.html', \
+            latitude=latitude, longitude=longitude, userid=userid, sndmap=sndmap)
+    elif latitude != None:
+        print latitude, longitude, userid
+        suggestions = service.run(latitude, longitude, userid)
+        sndmap = buildMap(suggestions)
+        return render_template('recommend.html', \
+            latitude=latitude, longitude=longitude, userid=userid, sndmap=sndmap)
     else:
         print "Default MAP Location"
         sndmap = Map(
@@ -130,7 +134,7 @@ def mapview():
               }
             ]
         )
-    return render_template('recommend.html', sndmap=sndmap)
+        return render_template('recommend.html', sndmap=sndmap)
 
 if __name__ == "__main__":
     app.run(debug=True, port=12345)
